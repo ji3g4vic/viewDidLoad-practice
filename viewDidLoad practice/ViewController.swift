bce5535d62dd4bc50150c2cf694a5bcf0b9ade9a
Vic <ji3g4vic03@gmail.com>
2022-12-18 15:26:11 +0800

    Initial Commit

---
+++ viewDidLoad practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewDidLoad practice
+//
+//  Created by 王衍智 on 2022/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
